15d10b43b227cc80aa0bb78733e726eff1ce4588
andrewaclark248 <andrewaclark248@gmail.com>
2023-06-14 20:53:12 -0400

    add auth

--- src/routes/api/userRoutes.ts
+++ src/routes/api/userRoutes.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 const userRoutes = express.Router();
 import { create, index, authenticate } from '../../controllers/usersController';
-
+import { verifyToken } from './../../utils/index'
 
 //users routes
 userRoutes.post('/users', create);
-userRoutes.get('/users', index);
+userRoutes.get('/users', verifyToken, index);
 userRoutes.post('/users/authenticate', authenticate);
 
 export default userRoutes;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,28 @@
 import jwt, { Secret } from "jsonwebtoken"
 import { User } from "../models/user"
+import {NextFunction, Request, Response} from "express"
 
 const SECRET = process.env.TOKEN_SECRET as Secret
 
 export function getTokenByUser (user: User) {
   return jwt.sign({user}, SECRET)
+}
+
+export function verifyToken(req: Request, res: Response, next: NextFunction): void | boolean {
+  //if token missing respond with access denied
+  if (!req.headers.authorization) {
+    res.status(401)
+    res.json("Access denied, invalid token")
+    return false
+  }
+
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    jwt.verify(token, SECRET);
+    next();
+  } catch (error) {
+    res.status(401);
+    res.json('Access denied, invalid token');
+    return;
+  }
 }
